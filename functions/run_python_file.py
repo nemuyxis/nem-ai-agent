5b1089f96cb3cfbfb75c8ab43e02097c271386d8
nemuyxis <nemu.yxis@gmail.com>
2026-03-23 01:09:48 -0400

    test_run_python_file

---
+++ functions/run_python_file.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=None):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        if os.path.isfile(target_dir):
+            valid_target_dir = os.path.commonpath([working_dir_abs, target_dir])
+            if valid_target_dir == working_dir_abs:
+                if file_path.endswith(".py"):
+                    command = ["python", target_dir]
+                    if args is not None:
+                        command.extend(args)
+                    Completedrun = subprocess.run(command, capture_output=True, text=True, timeout=30)
+                    run_out = ""
+                    if Completedrun.returncode != 0:
+                        run_out = f"Process exited with code {Completedrun.returncode}. "
+                    if Completedrun.stderr == '' and Completedrun.stdout =='':
+                        run_out += "No output produced. "
+                    else:
+                        run_out += f'STDOUT: {Completedrun.stdout} STDERR: {Completedrun.stderr}'
+                    print(run_out)
+                    return run_out
+                else:
+                    print(f'Error: "{file_path}" is not a Python file')
+                    return f'Error: "{file_path}" is not a Python file'
+            else:
+                print(f'Cannot execute "{file_path}" as it is outside the permitted working directory')
+                return f'Cannot execute "{file_path}" as it is outside the permitted working directory'
+        else:
+            print(f'Error: "{file_path}" does not exist or is not a regular file')
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
---
+++ test_run_python_file.py
@@ -0,0 +1,8 @@
+from functions.run_python_file import run_python_file
+
+run_python_file("calculator", "main.py") #(should print the calculator's usage instructions)
+run_python_file("calculator", "main.py", ["3 + 5"]) #(should run the calculator... which gives a kinda nasty rendered result)
+run_python_file("calculator", "tests.py") #(should run the calculator's tests successfully)
+run_python_file("calculator", "../main.py") #(this should return an error)
+run_python_file("calculator", "nonexistent.py") #(this should return an error)
+run_python_file("calculator", "lorem.txt") #(this should return an error)
