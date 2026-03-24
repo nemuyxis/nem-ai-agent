a91697638c99c14bd76aa109eeb97c45a1278916
nemuyxis <nemu.yxis@gmail.com>
2026-03-24 00:13:49 -0400

    new chapter

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+from prompts import system_prompt
+
 from google import genai
 
 client = genai.Client(api_key=api_key)
@@ -21,7 +23,8 @@ messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)]
 
 response = client.models.generate_content(
     model='gemini-2.5-flash',
-    contents=messages
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0),
 )
 if args.verbose:
     print(f"User prompt: {args.user_prompt}")
