d0229099537461d0f9b9fb0680cc9c8825a14de9
nemuyxis <nemu.yxis@gmail.com>
2026-03-25 00:17:12 -0400

    call functions step 1

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,20 @@
 import os
+from google.genai import types
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in a specified directory relative to the working directory, providing file size and directory status",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="Directory path to list files from, relative to the working directory (default is the working directory itself)",
+            ),
+        },
+    ),
+)
+
 def get_files_info(working_directory, directory="."):
     working_dir_abs = os.path.abspath(working_directory)
     target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
@@ -15,9 +31,12 @@ def get_files_info(working_directory, directory="."):
                 size = os.path.getsize(target_dir + "/" + dir)
                 is_dir = os.path.isdir(target_dir + "/" + dir)
                 str_dirs += f"  - {name}: file_size={size}, is_dir={is_dir}\n"
+            print(str_dirs)
             return str_dirs
         else:
+            print(f'Result for {directory} directory:\n     Error: Cannot list "{directory}" as it is outside the permitted working directory')
             return f'Result for {directory} directory:\n     Error: Cannot list "{directory}" as it is outside the permitted working directory'
     else:
+        print(f'Error: "{directory}" is not a directory')
         return f'Error: "{directory}" is not a directory'
     
--- main.py
+++ main.py
@@ -21,16 +21,23 @@ from google.genai import types
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
+from call_function import available_functions
+
 response = client.models.generate_content(
     model='gemini-2.5-flash',
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0),
+    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0),
 )
 if args.verbose:
     print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-print(response.text)
+
+if response.function_calls is None:
+    print(response.text)
+else:
+    for function_call in response.function_calls:
+        print(f"Calling function: {function_call.name}({function_call.args})")
 
 def main():
     print("Hello from your-project-name!")
